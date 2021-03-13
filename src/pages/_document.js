7d384ac2bf29931caf90a77f20944c95ad9fb36a
Weslley <weslley_mattosibf@hotmail.com>
2021-03-13 11:27:03 -0300

    document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import { Head, Html } from 'next/document'
+import Document, { Head, Html,Main, NextScript } from 'next/document'
 
 export default class Mydocument extends Document {
   render() {
@@ -8,6 +8,10 @@ export default class Mydocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,300&display=swap" rel="stylesheet" />
         </Head>
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
       </Html>
     )
   }
